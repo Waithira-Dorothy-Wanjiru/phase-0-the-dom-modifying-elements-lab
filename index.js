5269ca63db7c874bf1c0451dcfb5e0f3dfb9b3c4
Dorothy <dorothywanjiru32@gmail.com>
2024-10-14 01:23:15 +0300

    modifying

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your code here!
+
+const main = document.querySelector('main#main');
+if (main) {
+  main.remove();
+}
+
+const newHeader = document.createElement('h1');
+
+
+newHeader.id = 'victory';
+
+
+newHeader.innerHTML = 'YOUR-NAME is the champion'; 
+
+
+document.body.appendChild(newHeader);
